b384a5efe7d8d5139cf23c85d8c444d781d1540f
SeA <Iceseas@users.noreply.github.com>
2021-05-12 11:39:57 +0800

    index

---
+++ precache-manifest.469fb99ce5a615490bac3d2f078210ab.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54acbd2a21e37b0b32f5",
+    "url": "/kiwiui/css/chunk-0ec95e6b.a1c4d536.css"
+  },
+  {
+    "revision": "765ce153ca85a0723142",
+    "url": "/kiwiui/css/index.7acb4896.css"
+  },
+  {
+    "revision": "c5a7e5a0782658b821760c8951943572",
+    "url": "/kiwiui/home.ico"
+  },
+  {
+    "revision": "8b6e11485826299e0282abb3596b59e6",
+    "url": "/kiwiui/ice-cream.png"
+  },
+  {
+    "revision": "6ea22f11ad2ecab01a9caa31bdc4faad",
+    "url": "/kiwiui/img/wiki.6ea22f11.png"
+  },
+  {
+    "revision": "db1ef67130ffc7e9be0b19608c0d266b",
+    "url": "/kiwiui/index.html"
+  },
+  {
+    "revision": "54acbd2a21e37b0b32f5",
+    "url": "/kiwiui/js/chunk-0ec95e6b.03d57ae5.js"
+  },
+  {
+    "revision": "a4c68b06eff9d4ee5405",
+    "url": "/kiwiui/js/chunk-2d0bd267.b5bd57e9.js"
+  },
+  {
+    "revision": "335bd62c44cfb2d8ff97",
+    "url": "/kiwiui/js/chunk-2d0bdcb0.9f569f77.js"
+  },
+  {
+    "revision": "3722ccc2c5f890c2e9c9",
+    "url": "/kiwiui/js/chunk-2d0c7353.6b7e9f55.js"
+  },
+  {
+    "revision": "7cec72fd8ccf8b8dff66",
+    "url": "/kiwiui/js/chunk-2d0d4017.87703c8c.js"
+  },
+  {
+    "revision": "a7418c431cde3460de66",
+    "url": "/kiwiui/js/chunk-2d0e144f.6fb392a8.js"
+  },
+  {
+    "revision": "6ece7a803015feeca78e",
+    "url": "/kiwiui/js/chunk-2d216261.2b69ece8.js"
+  },
+  {
+    "revision": "9f6c2030e89e6c7fe50c",
+    "url": "/kiwiui/js/chunk-2d22a14d.2384ccba.js"
+  },
+  {
+    "revision": "ab0b6545f447b6126b9f",
+    "url": "/kiwiui/js/chunk-vendors.28992539.js"
+  },
+  {
+    "revision": "765ce153ca85a0723142",
+    "url": "/kiwiui/js/index.10107049.js"
+  },
+  {
+    "revision": "e4bedfb8386aa31c61cb0d0a957cf29e",
+    "url": "/kiwiui/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/kiwiui/robots.txt"
+  },
+  {
+    "revision": "6ea22f11ad2ecab01a9caa31bdc4faad",
+    "url": "/kiwiui/wiki.png"
+  }
+]);
